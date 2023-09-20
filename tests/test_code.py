f442fa7f810aa8cdd51a1b07769ab7d7ea36e7c2
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2023-09-20 13:04:27 +0000

    update

--- tests/test_code.py
+++ tests/test_code.py
@@ -13,11 +13,13 @@ import zarr
 from ecml_tools.data import Concat, Join, open_dataset
 
 
-def ij(i, j):
-    return i * 10000 + j
+def ij(i, j, k=0):
+    return i * 100 + j + k * 1_000_000
 
 
-def create_zarr(vars=["2t", "msl", "10u", "10v"], start=2021, end=2021, frequency=6):
+def create_zarr(
+    vars=["2t", "msl", "10u", "10v"], start=2021, end=2021, frequency=6, k=0
+):
     root = zarr.group()
 
     dates = []
@@ -31,7 +33,7 @@ def create_zarr(vars=["2t", "msl", "10u", "10v"], start=2021, end=2021, frequenc
     data = np.zeros((len(dates), len(vars)))
     for i in range(len(dates)):
         for j in range(len(vars)):
-            data[i, j] = ij(i, j)
+            data[i, j] = ij(i, j, k)
 
     root.data = data
     root.dates = dates
@@ -42,39 +44,38 @@ def create_zarr(vars=["2t", "msl", "10u", "10v"], start=2021, end=2021, frequenc
 
 
 def test_code():
-    root = create_zarr()
-    z = open_dataset(root)
-    print(len(z))
-    print(z.dates)
+    ds = open_dataset(create_zarr())
+    print(len(ds))
+    print(ds.dates)
 
-    for i, e in enumerate(z):
+    for i, e in enumerate(ds):
         print(i, e)
         if i > 10:
             break
 
 
 def test_concat():
-    z = open_dataset(
+    ds = open_dataset(
         create_zarr(start=2021, end=2021),
         create_zarr(start=2022, end=2022),
     )
-    assert isinstance(z, Concat)
-    assert len(z) == 365 * 2 * 4
-    for i, row in enumerate(z):
+    assert isinstance(ds, Concat)
+    assert len(ds) == 365 * 2 * 4
+    for i, row in enumerate(ds):
         n = i if i < 365 * 4 else i - 365 * 4
         expect = [ij(n, 0), ij(n, 1), ij(n, 2), ij(n, 3)]
         assert (row == expect).all()
 
 
 def test_join():
-    z = open_dataset(
+    ds = open_dataset(
         create_zarr(vars=["a", "b", "c", "d"]),
         create_zarr(vars=["e", "f", "g", "h"]),
     )
 
-    assert isinstance(z, Join)
-    assert len(z) == 365 * 4
-    for i, row in enumerate(z):
+    assert isinstance(ds, Join)
+    assert len(ds) == 365 * 4
+    for i, row in enumerate(ds):
         n = i
         expect = [
             ij(n, 0),
@@ -87,6 +88,24 @@ def test_join():
             ij(n, 3),
         ]
         assert (row == expect).all()
+
+
+def test_subset_1():
+    z = create_zarr(start=2021, end=2023, frequency=1)
+    ds = open_dataset(z, frequency=12)
+    assert len(ds) == 365 * 3 * 2
+
+
+def test_subset_2():
+    z = create_zarr(start=2021, end=2023, frequency=1)
+    ds = open_dataset(z, start=2022, end=2022)
+    assert len(ds) == 365 * 24
+
+
+def test_subset_3():
+    z = create_zarr(start=2021, end=2023, frequency=1)
+    ds = open_dataset(z, start=2022, end=2022, frequency=12)
+    assert len(ds) == 365 * 2
 
 
 if __name__ == "__main__":
