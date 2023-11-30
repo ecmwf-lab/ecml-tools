f253363b25c890b97209e1467733399fe9c5e78c
Florian Pinault <Florian.Pinault@ecmwf.int>
2023-11-30 10:04:41 +0000

    fix

--- scripts/speed-test.py
+++ scripts/speed-test.py
@@ -13,18 +13,13 @@ def main():
         "path",
         help="Path to the dataset, use s3:// for S3 storage, http:// for HTTP storage, and /path/to/dataset for local storage",
     )
+    
     args = parser.parse_args()
 
     ds = open_dataset(args.path)
 
     for i in tqdm(ds, total=len(ds), smoothing=0):
         pass
-        args = parser.parse_args()
-
-        ds = open_dataset(args.path)
-
-        for i in tqdm(ds, total=len(ds), smoothing=0):
-            pass
 
 
 if __name__ == "__main__":
