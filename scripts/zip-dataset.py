4bfa48dad597d4e77d3dd3e033847deabb9c488e
Florian Pinault <Florian.Pinault@ecmwf.int>
2023-12-07 09:20:25 +0000

    up

--- scripts/zip-dataset.py
+++ scripts/zip-dataset.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import zipfile
+import time
+import shutil
 from tqdm import tqdm
 import json
 import glob
@@ -26,11 +28,20 @@ def ensure_dir_exists(path):
 VERSION = "0.1"
 
 
+def to_human_readable(seconds):
+    if seconds < 60:
+        return f"{seconds:.2f} seconds"
+    elif seconds < 60 * 60:
+        return f"{seconds / 60:.2f} minutes"
+    else:
+        return f"{seconds / 60 / 60:.2f} hours"
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=(
-            "This script zips a zarr dataset into a unique zip file. "
-            "Mostly robust to polite interuptions (ctrl+C SIGTERM) "
+            "This script zips a directory dataset into a unique zip file. "
+            "Mostly robust to interuptions (ctrl+C or SIGTERM) "
             "and can run incrementally."
         )
     )
@@ -38,22 +49,28 @@ def main():
     parser.add_argument("--target", help="Target argument", required=True)
     args = parser.parse_args()
 
-    assert args.source.endswith(".zarr")
-    assert args.target.endswith(".zip")
+    target = args.target
+    tmptarget = target + ".tmp"
+    if os.path.exists(target):
+        print(f"❌ Target {target} already exists, exiting.")
+        sys.exit(1)
 
-    ensure_dir_exists(args.target)
+    ensure_dir_exists(target)
 
-    # list all files in the source directory
+    print(f"Listing files in {args.source}")
+    start = time.time()
     files = []
-    for root, _, filenames in os.walk(args.source):
+    for root, _, filenames in tqdm(os.walk(args.source)):
         for filename in filenames:
             path = os.path.join(root, filename)
             if os.path.isdir(path):
                 continue
             relpath = os.path.relpath(path, args.source)
             files.append(relpath)
-    files = sorted(files)
-    print(f"Found {len(files)} files in {args.source}")
+    end = time.time()
+    print(
+        f"Found {len(files)} files in {args.source} (took {to_human_readable(end-start)})"
+    )
 
     def open_zip(path, mode):
         return zipfile.ZipFile(
@@ -78,20 +95,21 @@ def main():
         zf.comment = comment.encode("utf-8")
         zf.close()
 
-    if not os.path.exists(args.target):
-        print(f"Target {args.target} does not exist, creating it.")
-        with open(args.target, mode="wb") as f:
+    if not os.path.exists(tmptarget):
+        print(f"Target {tmptarget} does not exist, creating it.")
+        with open(tmptarget, mode="wb") as f:
             f.write("ZARR_ZIP\n".encode("utf-8"))
-        zf = open_zip(args.target, mode="a")
+        zf = open_zip(tmptarget, mode="a")
         add_metadata(zf, latest_file_written=None)
         zf.close()
+    else:
+        print(f"Target {tmptarget} already exists, appending to it.")
 
-    metadata = read_metadata(args.target)
+    metadata = read_metadata(tmptarget)
     print(metadata)
-    zf = open_zip(args.target, mode="a")
+    zf = open_zip(tmptarget, mode="a")
     already_exists = zf.namelist()
-    print(f"Found {len(already_exists)} files already in {args.target}")
-    print(os.getpid())
+    print(f"Found {len(already_exists)} files already in {tmptarget}")
 
     global stop
     stop = None
@@ -103,6 +121,7 @@ def main():
         stop = signal_name
 
     signal.signal(signal.SIGTERM, handle_signal)
+    signal.signal(signal.SIGALRM, handle_signal)
     signal.signal(signal.SIGINT, handle_signal)
 
     for relpath in tqdm(files):
@@ -119,7 +138,7 @@ def main():
                 raise Exception(f"Received {stop} signal")
         except Exception as e:
             print(f"An error occurred while writing to the zip file: {e}")
-            print(f"Closing the zip file {args.target} and exiting...")
+            print(f"Closing the zip file {tmptarget} and exiting...")
             add_metadata(zf, latest_file_written=relpath)
             zf.close()
             raise
@@ -128,7 +147,10 @@ def main():
     zf.close()
 
     signal.signal(signal.SIGTERM, signal.SIG_DFL)
+    signal.signal(signal.SIGALRM, signal.SIG_DFL)
     signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    shutil.move(tmptarget, target)
 
 
 if __name__ == "__main__":
