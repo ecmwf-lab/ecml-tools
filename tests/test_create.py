27d37eb1a8d8c99a36f1611d7d05bf9231804ed8
Florian Pinault <Florian.Pinault@ecmwf.int>
2024-01-30 15:32:07 +0000

    added pytest to create datasets

--- tests/test_create.py
+++ tests/test_create.py
@@ -7,62 +7,89 @@
 
 import os
 import tempfile
+import json
 
 from ecml_tools.create import Creator
 from ecml_tools.data import open_dataset
 
 
-def _config_path(name):
-    here = os.path.dirname(__file__)
-    return os.path.join(here, name + ".yaml")
+def compare_dot_zattrs(a, b):
+    if isinstance(a, dict):
+        for k in a:
+            assert k in b
+            if k in ["timestamp", "uuid", "latest_write_timestamp"]:
+                assert type(a[k]) == type(b[k]), (type(a[k]), type(b[k]), a[k], b[k])
+            return compare_dot_zattrs(a[k], b[k])
+
+    if isinstance(a, list):
+        assert len(a) == len(b), (a, b)
+        for v, w in zip(a, b):
+            return compare_dot_zattrs(v, w)
+
+    assert type(a) == type(b), (type(a), type(b), a, b)
+    return a == b, (a, b)
 
 
-def _output_path(name):
-    here = os.path.dirname(__file__)
-    return os.path.join(here, name + "-output")
-
-
-def _reference_path(name):
-    here = os.path.dirname(__file__)
-    return os.path.join(here, name + "-reference")
-
-
-def compare(dir1, dir2, skip=[".zattrs", "provenance_load.json"]):
+def compare(dir1, dir2):
     """Compare two directories recursively."""
-    for file1 in os.listdir(dir1):
-        path1 = os.path.join(dir1, file1)
-        path2 = os.path.join(dir2, file1)
-        if os.path.isfile(path1):
-            assert os.path.isfile(path2), f"File {path2} does not exist"
-            if file1 in skip:
-                continue
-            with open(path1, "rb") as f1:
-                with open(path2, "rb") as f2:
-                    assert f1.read() == f2.read(), f"{path1} != {path2}"
-        else:
+    files1 = set(os.listdir(dir1))
+    files2 = set(os.listdir(dir2))
+    files = files1.union(files2)
+    
+    for f in files:
+        path1 = os.path.join(dir1, f)
+        path2 = os.path.join(dir2, f)
+
+        if not os.path.isfile(path1):
             assert os.path.isdir(path2), f"Directory {path2} does not exist"
             compare(path1, path2)
+            continue
+
+        assert os.path.isfile(path2), f"File {path2} does not exist"
+
+        content1 = open(path1,'rb').read()
+        content2 = open(path2,'rb').read()
+
+        if f == ".zattrs":
+            compare_dot_zattrs(json.loads(content1), json.loads(content2))
+            continue
+
+        if f == "provenance_load.json":
+            assert False, "test not implemented to compare temporary statistics"
+
+        assert content1 == content2, f"{path1} != {path2}"
 
 
-def test_create_1():
-    output = _output_path("create-1")
-    reference = _reference_path("create-1")
-    config = _config_path("create-1")
+def _test_create(name):
+    here = os.path.dirname(__file__)
+    config = os.path.join(here, name + ".yaml")
+    output = os.path.join(here, name + "-output", name + ".zarr")
+    reference = os.path.join(here, name + "-reference", name + ".zarr")
 
-    zarr_path = os.path.join(output, "create-1.zarr")
-
-    c = Creator(zarr_path, config=config, cache=None, overwrite=True)
-    # statistics_tmp=None,
+    c = Creator(output, config=config, cache=None, overwrite=True)
     c.create()
 
-    compare(output, reference)
-
     # ds = open_dataset(zarr_path)
-    # assert ds.shape == (10, 8, 1, 162)
-    # assert ds.variables == (10, 8, 1, 162)
+    # assert ds.shape == 
+    # assert ds.variables == 
+
+    compare(reference, output)
+
+
+
+def test_create_concat():
+    _test_create("create-concat")
+
+
+def test_create_join():
+    _test_create("create-join")
+
+
+def test_create_pipe():
+    _test_create("create-pipe")
 
 
 if __name__ == "__main__":
     import sys
 
-    test_create_1(sys.argv[1])
+    _test_create(sys.argv[1])
