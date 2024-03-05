9f8cd5d4c3d6d8b21592dbe9a10a01809e52ff11
Florian Pinault <Florian.Pinault@ecmwf.int>
2024-03-05 20:28:23 +0000

    added variables_with_nans in dataset metadata

--- ecml_tools/create/loaders.py
+++ ecml_tools/create/loaders.py
@@ -190,9 +190,7 @@ class InitialiseLoader(Loader):
         variables = self.minimal_input.variables
         self.print(f"Found {len(variables)} variables : {','.join(variables)}.")
 
-        variables_with_nans = self.minimal_input.variables_with_nans
-        if variables_with_nans:
-            print(f"  Found nans for {' '.join(variables_with_nans)}")
+        variables_with_nans = self.config.get("has_nans", [])
 
         ensembles = self.minimal_input.ensembles
         self.print(f"Found {len(ensembles)} ensembles : {','.join([str(_) for _ in ensembles])}.")
@@ -236,6 +234,7 @@ class InitialiseLoader(Loader):
 
         metadata["ensemble_dimension"] = len(ensembles)
         metadata["variables"] = variables
+        metadata["variables_with_nans"] = variables_with_nans
         metadata["resolution"] = resolution
 
         metadata["licence"] = self.main_config["licence"]
